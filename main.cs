ddb07bf93d4f110d91cf84dd82cb2f10f3e2e97d
HigorParnoff <55004701+HigorParnoff@users.noreply.github.com>
2019-10-03 21:47:40 -0300

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,43 @@
+using System;
+
+/*
+Praticar é viver!
+
+Crie um mini projeto de qualquer natureza contendo as 
+seguintes especificações:
+ - Duas Classes;
+ - Ao menos 3 Atributos para cada clase 
+ (com pelo menos 1 getter e Setter);
+ - 3 Métodos cada;
+ - Ao menos 2 construtores cada (vazio e com parâmetros)
+ - Torne a relação das classes uma relação de dependência(
+ Uma classe contém a Outra);
+ - Crie ao menos um método contendo uma manipulação de 
+ Lista (Add() ) 
+ - Crie ao menos um método passando uma classe como 
+ parâmetro e manipulando os atributos delas
+ 
+ Use: tiny.cc/cartaoExemplo6 como ajuda
+ 
+ Crie um repositório em seu GitHub para abarcar a 
+ solução e faça ao menos 1 commit;
+ */
+
+// Classe Estoque
+//Atributos:Conteiner(tamanho pequeno espaço), mapa container(mapa da sala), 
+
+//metodos:Esteira,  localizar local(onde guardar), classificar(codigo);
+//
+// classe Braço Robótico.
+// metodos: posicionar(coordenadas); garra(bool);  leitor de codigo;
+
+
+namespace estoque {
+  class MainClass {
+    public static void Main (string[] args) {
+      Console.WriteLine ("Hello World");
+    }
+  }
+}
+
+https://github.com/HigorParnoff/Mini_Projeto_AED/invitations
